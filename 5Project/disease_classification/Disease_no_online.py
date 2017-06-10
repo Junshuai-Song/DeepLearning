e71cba5f9b66311452b508bf74fecd376935d48a
songjs <1390880230@qq.com>
2017-06-10 11:21:06 +0800

    final

--- 5Project/disease_classification/Disease_no_online.py
+++ 5Project/disease_classification/Disease_no_online.py
@@ -1,12 +1,7 @@
 # Auther: Alan
 """
-还没试，因为之前Disease.py没收敛
-
-现在好像都收敛不了，尝试下VGG吧。
-
-随机切割。。
+不随机切割
 """
-
 import tensorflow as tf
 import random
 import os
@@ -155,10 +150,22 @@ class Disease:
             # print("file:", batch)
             lena = Image.open("./data/G0/" + batch)
 
-            first = random.sample([j for j in range(100)], 1)[0]
-            second = random.sample([j for j in range(100)], 1)[0]
-            lena = lena.crop((first, second, first + 600, second + 600))
+            # first = random.sample([j for j in range(100)], 1)[0]
+            # second = random.sample([j for j in range(100)], 1)[0]
+            # lena = lena.crop((first, second, first + 600, second + 600))
 
+            # ~ add train samples
+            """
+            rotate_rate = random.sample([j for j in range(100)], 1)[0]
+            if rotate_rate<= 25:
+                lena = lena
+            elif rotate_rate <=50:
+                lena = lena.rotate(90)
+            elif rotate_rate <= 75:
+                lena = lena.rotate(180)
+            else:
+                lena = lena.rotate(270)
+            """
             # lena.save("temp.jpg")
             # lena = Image.open("temp.jpg")
             # print("new image.size:", lena.size)
@@ -167,32 +174,43 @@ class Disease:
             batch_ys.append(self.get_one_hot(batch_ys_[i], 3))      # one_hot
         return np.array(batch_xs), np.array(batch_ys)
 
-    def get_test(self, testX, testY):
-        # get test data in memory.
-        batch_xs, batch_ys = [], []
-        for i in range(testX.shape[0]):
-            if i%50==0:
-                print("test i:", i)
-            batch = testX[i]
-            lena = Image.open("./data/G0/" + batch)
-            first, second = 50, 50
-            lena = lena.crop((first, second, first + 600, second + 600))
-            batch_xs.append(np.reshape(lena, [-1]) / 255.0)
-            batch_ys.append(self.get_one_hot(testY[i], 3))      # one_hot
+    def next_batch_train(self, trainX, trainY, minibatch, num_examples):
+
+        # l_0,l_1,l_2 = [], [], []
+        # for i in range(trainY.shape[0]):
+        #     if trainY[i][0]==1:
+        #         l_0.append(i)
+        #     elif trainY[i][1]==1:
+        #         l_1.append(i)
+        #     else:
+        #         l_2.append(i)
+        # # print(l_0, l_1)
+        # # print("the length of l_s: ", len(l_0), len(l_1), len(l_2))
+        # locations = random.sample(l_0, int(minibatch/3.0))
+        # locations1 = random.sample(l_1, int(minibatch / 3.0))
+        # locations2 = random.sample(l_2, int(minibatch / 3.0))
+        # # print("locations: ", locations1)
+        #
+        # locations.extend(locations1)
+        # locations.extend(locations2)
+        locations = random.sample([i for i in range(len(trainX))], minibatch)
+        batch_xs = trainX[locations]
+        batch_ys = trainY[locations]
         return np.array(batch_xs), np.array(batch_ys)
 
     def my_image_filter(self, x_image):
         self.h_conv1 = tf.nn.relu(self.conv2d(x_image, self.W_conv1) + self.b_conv1)
-        self.h_pool1 = self.max_pool_2x2(self.h_conv1)  # 300*300*32
+        self.h_pool1 = self.max_pool_2x2(self.h_conv1)  # 350*350*32
         self.h_conv2 = tf.nn.relu(self.conv2d(self.h_pool1, self.W_conv2) + self.b_conv2)
-        self.h_pool2 = self.max_pool_5x5(self.h_conv2)  # 60*60*32
+        self.h_pool2 = self.max_pool_5x5(self.h_conv2)  # 70*70*32
         self.h_conv3 = tf.nn.relu(self.conv2d(self.h_pool2, self.W_conv3) + self.b_conv3)
 
+        # attribute
         self.h_conv4 = tf.nn.relu(self.conv2d(self.h_conv3, self.W_conv4_attr) + self.b_conv4_attr)
-        self.h_pool4 = self.max_pool_2x2(self.h_conv4)  # 30*30*64
+        self.h_pool4 = self.max_pool_2x2(self.h_conv4)  # 35*35*64
         self.h_conv5 = tf.nn.relu(self.conv2d(self.h_pool4, self.W_conv5_attr) + self.b_conv5_attr)
-        self.h_pool5 = self.max_pool_5x5(self.h_conv5)  # 6*6
-        self.h_flat1 = tf.reshape(self.h_pool5, [-1, 6 * 6 * 8])
+        self.h_pool5 = self.max_pool_5x5(self.h_conv5)  # 7*7
+        self.h_flat1 = tf.reshape(self.h_pool5, [-1, 7 * 7 * 8])
 
         self.h_flat1_drop = tf.nn.dropout(self.h_flat1, self.keep_prob)
         self.h_fc1 = tf.matmul(self.h_flat1_drop, self.W_fc1_attr) + self.b_fc1_attr
@@ -203,19 +221,25 @@ class Disease:
         return self.y_conv
 
 
-    def model(self, input_file, log_path):
-        trainX, trainY, testX, testY = self.read_data_disease(input_file)
+    def model(self, train_file, log_path):
+        trainX, trainY, testX, testY = self.read_data_disease(train_file)
+        print("trainX.shape: ", trainX.shape, trainY.shape, testX.shape, testY.shape)
+        trainX, trainY = self.next_batch(trainX, trainY, trainX.shape[0], trainX.shape[0])
+
+        # test small size train set.
+        # trainX = trainX[0:300]
+        # trainY = trainY[0:300]
         cnt = 0
         for i in testY:
             if i == 0:
                 cnt += 1
         print("minimal precision: ", float(cnt) / testY.shape[0])
-        # testX,testY = self.get_test(testX, testY, testX.shape[0], testY.shape[0])
+        testX,testY = self.next_batch(testX, testY, testX.shape[0], testY.shape[0])
 
 
         self.keep_prob = tf.placeholder(tf.float32)
-        self.x_ = tf.placeholder(tf.float32, [None, 600 * 600 * 3])  # shape [None, 128]
-        self.x = tf.reshape(self.x_, [-1, 600, 600, 3])
+        self.x_ = tf.placeholder(tf.float32, [None, 700 * 700 * 3])  # shape [None, 128]
+        self.x = tf.reshape(self.x_, [-1, 700, 700, 3])
 
         self.W_conv1 = self.weight_variable([5, 5, 3, 8])
         self.b_conv1 = self.bias_variable([8])
@@ -229,14 +253,14 @@ class Disease:
         self.b_conv4_attr = self.bias_variable([16])
         self.W_conv5_attr = self.weight_variable([3, 3, 16, 8])
         self.b_conv5_attr = self.bias_variable([8])
-        self.W_fc1_attr = self.weight_variable([6 * 6 * 8, 36])  # 288 * 36
+        self.W_fc1_attr = self.weight_variable([7 * 7 * 8, 36])  # 288 * 36
         self.b_fc1_attr = self.bias_variable([36])
 
         self.W_fc2 = self.weight_variable([36, 3])
         self.b_fc2 = self.bias_variable([3])
 
 
-        self.y_ = tf.placeholder(tf.float32, [None, 3])     # shape [None, 3]
+        self.y_ = tf.placeholder(tf.float32, [None, 3])     # shape [None, 250]
         self.y_conv = tf.nn.softmax(self.my_image_filter(self.x))
 
         # self.cross_entropy = tf.reduce_mean(-tf.reduce_sum(self.y_ * tf.log(self.y_conv + 1e-10), reduction_indices=[1]))
@@ -259,19 +283,22 @@ class Disease:
         self.merged_summary_op = tf.summary.merge_all()
 
         drops = [1.0]   # overfitting.
-        global_step = 201
-        for i in range(len(drops)):
-            drop = drops[i]
-            log_path = log_path + str(i)
+        global_step = 101
+        for d in range(len(drops)):
+            drop = drops[d]
+            log_path = log_path + str(d)
             print("log_path: ", log_path, "  drop:", drop)
 
-            sess = tf.Session()
+            gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.9)
+            sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+
+            # sess = tf.Session()
             sess.run(init_op)
-            summary_writer_train = tf.summary.FileWriter(log_path + "train", graph=tf.get_default_graph())
-            summary_writer_test = tf.summary.FileWriter(log_path + "test", graph=tf.get_default_graph())
+            summary_writer_train = tf.summary.FileWriter(log_path+"train", graph=tf.get_default_graph())
+            summary_writer_test = tf.summary.FileWriter(log_path+"test", graph=tf.get_default_graph())
 
             num_examples = trainX.shape[0]
-            minibatch = 30
+            minibatch = 60
             maxc = -1.0
             for epoch in range(global_step):
                 print("iter:", epoch)
@@ -280,11 +307,12 @@ class Disease:
                 total_batch = int(num_examples / minibatch)
                 # total_batch = 1
 
-                rate = 0.001 * math.pow(0.7, int(epoch/15))
+                rate = 0.001 * math.pow(0.7, int(epoch/3))
                 print("learning rate: ", rate)
 
                 for i in range(total_batch):
-                    batch_xs, batch_ys = self.next_batch(trainX, trainY, minibatch, num_examples)
+                    batch_xs, batch_ys = self.next_batch_train(trainX, trainY, minibatch, num_examples)
+                    # print(batch_xs.shape, batch_ys.shape)
 
                     # _, c, summary = sess.run([self.train_step, self.cross_entropy, self.merged_summary_op], feed_dict={self.x_: batch_xs, self.y_: batch_ys, self.learning_rate: rate, self.keep_prob: drop})
                     _, c , summary = sess.run([self.train_step, self.cross_entropy, self.merged_summary_op],
@@ -295,9 +323,8 @@ class Disease:
                     if i%1==0:
                         print("i/tot: ", i, "/", total_batch, "   current c:", c, "   ave_cost:", avg_cost)
 
-                    # test
-                    batch_xs, batch_ys = self.next_batch(testX, testY, minibatch, len(testY))
-                    # batch_xs, batch_ys = self.next_batch_train(testX, testY, minibatch, len(testX))
+                    # # test
+                    batch_xs, batch_ys = self.next_batch_train(testX, testY, minibatch, len(testX))
                     pre, summary = sess.run([self.accuracy, self.merged_summary_op],
                                             feed_dict={self.x_: batch_xs, self.y_: batch_ys, self.learning_rate: rate,
                                                        self.keep_prob: drop})
@@ -305,10 +332,11 @@ class Disease:
                     if pre > maxc:
                         maxc = pre
 
-
-                # if epoch % 5 == 0 and epoch > 40:
-                #     pre, summary = sess.run([self.accuracy, self.merged_summary_op], feed_dict={self.x_: testX, self.y_: testY, self.keep_prob: drop})
-                #     summary_writer.add_summary(summary, epoch * total_batch)
+                # test
+                # if epoch % 1 == 0:
+                #     batch_xs, batch_ys = self.next_batch_train(testX, testY, minibatch, len(testX))
+                #     pre, summary = sess.run([self.accuracy, self.merged_summary_op], feed_dict={self.x_: batch_xs, self.y_: batch_ys, self.learning_rate: rate, self.keep_prob: drop})
+                #     summary_writer_test.add_summary(summary, epoch * total_batch)
                 #
                 #     print("precision: ", pre)
                 #     if pre > maxc:
@@ -320,7 +348,7 @@ class Disease:
 if __name__ =="__main__":
     # CNN_LeNet_5_dev("./cifar_data/train.mat", "./cifar_data/test.mat", "./CNN/cifar")
     disease = Disease()
-    disease.model("./data/", "./log_2_new_")
+    disease.model("./data/", "./log_1_new_80_")
 
 
 
